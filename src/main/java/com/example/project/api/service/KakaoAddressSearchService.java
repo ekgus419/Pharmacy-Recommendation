86f78b316b2eefc0a48f7b204eb88215152025e4
ekgus419 <ekgus419@gmail.com>
2024-07-17 19:14:10 +0900

    카카오 주소 검색 api를 구현한다.

---
+++ src/main/java/com/example/project/api/service/KakaoAddressSearchService.java
@@ -0,0 +1,48 @@
+package com.example.project.api.service;
+
+import com.example.project.api.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoAddressSearchService {
+
+    private final RestTemplate restTemplate;
+
+    private final KakaoUriBuilderService kakaoUriBuilderService;
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
+    // 문자열 기반 주소를 입력 받아서 카카오 api 에 호출
+    // 호출 받은 응답값을 리턴
+    public KakaoApiResponseDto requestAddressSearch(String address) {
+
+        if (ObjectUtils.isEmpty(address)) return null;
+
+        // uri 생성
+        URI uri = kakaoUriBuilderService.buildUriByAddressSearch(address);
+
+        // api 규격에 맞게 세팅
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
+
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        // kakao api 호출
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+
+
+}
