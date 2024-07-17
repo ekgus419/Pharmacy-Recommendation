9736451920c61931328c021c3846b29e56c1ab2b
ekgus419 <ekgus419@gmail.com>
2024-07-18 02:09:03 +0900

    refactoring
    주석 추가

--- src/main/java/com/example/project/api/dto/DocumentDto.java
+++ src/main/java/com/example/project/api/dto/DocumentDto.java
@@ -6,18 +6,25 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+// 카카오 api 에서 리턴 받은 결과 값 중 필요한 데이터만 추출
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentDto {
 
+    @JsonProperty("place_name")
+    private String placeName; // 약국명
+
     @JsonProperty("address_name")
-    private String addressName;
+    private String addressName; // 주소
 
     @JsonProperty("y")
     private double latitude; // 위도
 
     @JsonProperty("x")
     private double longitude; // 경도
+
+    @JsonProperty("distance")
+    private double distance; // 카카오에서 위도,경도 계산하여 가까운 약국을 찾아줌
 }
--- src/main/java/com/example/project/api/dto/MetaDto.java
+++ src/main/java/com/example/project/api/dto/MetaDto.java
@@ -5,12 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+// 필요한 DTO 만 받아옴.
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class MetaDto {
 
     @JsonProperty("total_count")
-    private Integer totalCount;
+    private Integer totalCount; // 검색어에 검색된 문서 전체수
 
 }
